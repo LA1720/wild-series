35e978dd667eb40fb1dbdbe6789e24799f57e7ed
ALI AFZAL <layaafzal74@gmail.com>
2020-12-09 22:41:00 +0100

    quete3_affichage twig

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,46 @@
+<?php
+
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;  // this use is for @Route
+use Symfony\Component\HttpFoundation\Response;  // this use is for Response ()
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //this use is for AbstractController
+
+class ProgramController extends AbstractController  //here it's my first class extends by abstractController
+{
+    
+     /**
+     * @Route("/programs/", name="program_index")  // http://localhost:8000/programs/ doit renvoyer une page blanche avec écrit "Wild Series index". //
+     */
+
+
+    public function index(): Response  //c'est un objet qui représente une réponse HTTP complète. //
+    {
+         // To write html inside the methon index()//
+
+        // return new Response(
+        //     '<html><body>Wild Series Index</body></html>'
+        // ); 
+
+
+        //make the view program/index.html.twig in templats 
+        return $this->render('program/index.html.twig', [
+
+            'website' => 'wild series',
+
+        ]);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
