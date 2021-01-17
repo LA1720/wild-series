f82e525ed74ae21844714ba0b6b8513be009475b
ALI AFZAL <layaafzal74@gmail.com>
2021-01-17 17:22:12 +0100

    le routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,11 +7,14 @@ use Symfony\Component\Routing\Annotation\Route;  // this use is for @Route
 use Symfony\Component\HttpFoundation\Response;  // this use is for Response ()
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //this use is for AbstractController
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController  //here it's my first class extends by abstractController
 {
     
      /**
-     * @Route("/programs/", name="program_index")  // http://localhost:8000/programs/ doit renvoyer une page blanche avec écrit "Wild Series index". //
+     * @Route("/", name="index")  // http://localhost:8000/programs/ doit renvoyer une page blanche avec écrit "Wild Series index". //
      */
 
 
@@ -22,17 +25,25 @@ class ProgramController extends AbstractController  //here it's my first class e
         // return new Response(
         //     '<html><body>Wild Series Index</body></html>'
         // ); 
+        $yo = '5';
 
 
         //make the view program/index.html.twig in templates 
         return $this->render('program/index.html.twig', [
 
-            'website' => 'wild series',
+            'website' => $yo,
             'wild' => 'code school'
 
         ]);
 
 
+    }
+    /**
+     * @Route("/show/{id}", methods={"GET"}, requirements={"page"="\d+"}, name="show")
+     */
+    public function show (int $id): Response
+    {
+        return $this->render('program/show.html.twig', ["id"=> $id]);
     }
 }
 
